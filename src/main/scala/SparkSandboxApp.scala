8ddfdc03c1b9834a9fd1ab7d9b941a333232cd8b
Kamil Duda <kamilduda01@gmail.com>
2021-09-06 19:36:07 +0200

    Cleanup application

--- src/main/scala/SparkSandboxApp.scala
+++ src/main/scala/SparkSandboxApp.scala
@@ -2,15 +2,14 @@ package com.fineconstant.spark
 
 import infrastructure.SparkApplication
 
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.expressions._
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.IntegerType
 
 import scala.language.postfixOps
 
 object SparkSandboxApp extends SparkApplication {
   import sparkSession.implicits._
-
-
 
   sparkSession.stop()
 }
